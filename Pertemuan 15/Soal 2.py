a63fc452a281f808f5e678b83b4ca5d597e87045
Muhammad Atsaal <muhammadatsaal123@gmail.com>
2025-08-15 13:49:25 +0700

    first commit

--- Pertemuan 15/Soal 2.py
+++ Pertemuan 15/Soal 2.py
@@ -3,3 +3,17 @@
 # Tambahkan pasangan key-value baru: "kota": "Nama Kotamu".
 # Ubah value dari key "umur" menjadi umurmu tahun depan.
 # Cetak dictionary yang sudah diperbarui.
+
+
+biodata = {
+    "nama": "atsaal",
+    "umur": 18,
+    "hobi": ['Mancing','Ngaji','Lari'],
+    "sudah_menikah": True
+}
+
+# menambahkan key-value baru
+biodata["kota"] = "Cirebon"
+# mengganti key-value
+biodata["umur"] = 18
+print("Profil setelah ditambah:", biodata)
