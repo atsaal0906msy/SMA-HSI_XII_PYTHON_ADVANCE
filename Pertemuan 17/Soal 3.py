2700dde4fc0f8eb7805888a4a46f0948f6354653
Muhammad Atsaal <muhammadatsaal123@gmail.com>
2025-08-19 13:57:04 +0700

    perbaikan

--- Pertemuan 17/Soal 3.py
+++ Pertemuan 17/Soal 3.py
@@ -3,10 +3,9 @@
 # Gunakan tuple assignment untuk "membongkar" nilai tersebut ke dalam tiga variabel terpisah: red, green,
 # dan blue. Cetak ketiga variabel tersebut.
 
-# Membuat tuple
 warna = (255, 165, 0)
-black, pace, jawa = warna
-# "Membongkar" tuple ke dalam variabel x dan y
-print(f"Warna : {black}") 
-print(f"Warna pace: {pace}") 
-print(f"Warna jawa: {jawa}")   
+pink, hitam, jawa = warna
+
+print(f"Warna boti: {pink}") 
+print(f"Warna pace: {hitam}") 
+print(f"Warna hama: {jawa}")   
--- Pertemuan 17/Soal 4.py
+++ Pertemuan 17/Soal 4.py
@@ -5,16 +5,16 @@
 # papan_catur[(8, 'h')] = "Benteng Hitam"
 # Isi beberapa posisi, lalu akses dan cetak bidak yang ada di posisi (1, 'a').
 
-kalimat = input("Masukkan sebuah kalimat: ")
-kalimat = kalimat.lower()
-kata_kata = kalimat.split()
+papan_catur = {}
 
+papan_catur[(1, 'a')] = "Benteng Putih"
+papan_catur[(1, 'b')] = "Kuda Putih"
+papan_catur[(1, 'c')] = "Gajah Putih"
+papan_catur[(8, 'h')] = "Benteng Hitam"
+papan_catur[(8, 'g')] = "Kuda Hitam"
 
-histogram = {}
- 
-for kata in kata_kata:
-    if kata in histogram:
-        histogram[kata] += 1
-    else:
-        histogram[kata]
-print("Histogram kata:", histogram) 
+print("Bidak di (1, 'a') adalah:", papan_catur[(1, 'a')])
+
+print("\nIsi papan catur:")
+for posisi, bidak in papan_catur.items():
+    print(f"Posisi {posisi}: {bidak}")
